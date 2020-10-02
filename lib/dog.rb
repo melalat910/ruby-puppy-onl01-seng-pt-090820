3051a734271dc02f2d32ae5af5ea89fafd5570c5
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-02 00:24:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,14 +8,10 @@ class Dog
         @name = name
         @@all << self
     end
-
-    def self.all
-        @@all
-    end
     def self.clear_all
-        @@all
+        @@all.clear
     end
-    def self.print_all
-        @@all
+    def self.all
+        @@all.each { |dog_name| puts dog_name.name  }
     end
 end
