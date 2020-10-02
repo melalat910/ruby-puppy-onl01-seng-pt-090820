5c1fa831ecf65fab301ccaa61435191bf048b848
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-02 00:29:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,12 +6,23 @@ class Dog
 
     def initialize(name)
         @name = name
-        @@all << self
-    end
-    def self.clear_all
-        @@all.clear
+        self.save
     end
     def self.all
-        @@all.each { |dog_name| puts dog_name.name  }
+        @@all
     end
+    def self.clear_all
+        @@all = []
+    end
+    def self.print_all
+      @@all.each do |dog|
+        puts dog.name
+    end
+ end 
+def save
+  @@all << self
+end
+def name
+  @name
+end
 end
